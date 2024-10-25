c98e1fc0817d685dc79888574026a824298ae53e
icarodredd <icarog854@gmail.com>
2024-10-25 09:11:44 -0300

    add sidebar test

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,10 @@ export default function Sidebar() {
     <section className="h-screen w-40 font-inter max-lg:h-min max-md:flex max-lg:items-center max-lg:justify-between max-md:w-screen">
       <div className="flex justify-center items-center gap-4 p-4">
         <Avatar>
-          <AvatarImage src={logo} />
+          <AvatarImage
+            alt="Craftlog"
+            src={logo}
+          />
           <AvatarFallback>CL</AvatarFallback>
         </Avatar>
         <h1>Craftlog</h1>
--- tests/Sidebar.spec.tsx
+++ tests/Sidebar.spec.tsx
@@ -3,10 +3,17 @@ import Sidebar from "../src/components/Sidebar";
 import "@testing-library/jest-dom/vitest";
 
 describe("Sidebar", () => {
-  it("should display a headind with text of Craftlog", () => {
+  it("should display a heading with text of Craftlog", () => {
     render(<Sidebar />);
-
     const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent("Craftlog");
+  });
+
+  it("should display a button for dashboard", () => {
+    render(<Sidebar />);
+    const dashboardButton = screen.getByRole("button");
+    expect(dashboardButton).toBeInTheDocument();
+    expect(dashboardButton).toHaveTextContent("Dashboard");
   });
 });
