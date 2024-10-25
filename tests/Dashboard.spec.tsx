478b45ca574f7a2b65b4bdf0a46e1c9bf60940d3
icarodredd <icarog854@gmail.com>
2024-10-25 09:33:28 -0300

    add dashboard test

---
+++ tests/Dashboard.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import Dashboard from "../src/components/Dashboard";
+import "@testing-library/jest-dom/vitest";
+
+describe("Dashboard", () => {
+  it("should display a button for select the teams", () => {
+    render(<Dashboard />);
+    const button = screen.getByRole("combobox");
+    expect(button).toBeInTheDocument();
+  });
+  it("should display a greeting", () => {
+    render(<Dashboard />);
+    const greeting = screen.getByRole("heading");
+    expect(greeting).toBeInTheDocument();
+    expect(greeting).toHaveTextContent("Hello, member of Nexa Team!");
+  });
+  it("should display a button for add a task", () => {
+    render(<Dashboard />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Add Task");
+  });
+  it("should display a subtitle", () => {
+    render(<Dashboard />);
+    const paragraph = screen.getByRole("paragraph");
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph).toHaveTextContent("Tasks of the project:");
+  });
+});
